35bcbfa13141ce632c37dd0f8c9b7657d8cb16a8
Ken <ken@bradsdeals.com>
2013-08-04 19:58:11 -0500

    Adding procfile. Formatting cleanup.

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http')
 	, app = require('./app')
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });
